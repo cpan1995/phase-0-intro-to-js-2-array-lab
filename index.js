1e1fb4a6213efacb7d6629897c81fda13a6fd24c
cpan1995 <cpan1995@gmail.com>
2021-06-11 18:17:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+// Write your solution here
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(n){
+    return cats.push(n);
+}
+
+function destructivelyPrependCat(n){
+    return cats.unshift(n);
+}
+
+function destructivelyRemoveLastCat(n){
+    return cats.pop(n);
+}
+
+function destructivelyRemoveFirstCat(n){
+    return cats.shift(n);
+}
+
+function appendCat(n){
+    const newCats = [...cats, n];
+    return newCats;
+}
+
+function prependCat(n){
+    const newCats = [n, ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0, cats.length - 1);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
+}
